ce34db4c31c79d4a8bd417d7678b64e045b752c2
iolpji <jihyeong00@naver.com>
2022-11-20 01:49:41 +0900

    메인페이지 완료

--- Project/main/js/swiper.js
+++ Project/main/js/swiper.js
@@ -1,46 +1,24 @@
-      var swiper = new Swiper(".mySwiper", {
-        spaceBetween: 30,    // 슬라이드 사이 여백
-
-        slidesPerView : 'auto', // 한 슬라이드에 보여줄 갯수
-        centeredSlides: true,    //센터모드
-        autoplay: {     //자동슬라이드 (false-비활성화)
-          delay: 2500, // 시간 설정
-          disableOnInteraction: false, // false-스와이프 후 자동 재생
-        },
-
-        loop : false,   // 슬라이드 반복 여부
-
-        loopAdditionalSlides : 1,
-
- // 슬라이드 반복 시 마지막 슬라이드에서 다음 슬라이드가 보여지지 않는 현상 수정
-        pagination: { // 호출(pager) 여부
-          el: ".swiper-pagination", //버튼을 담을 태그 설정
-          clickable: true, // 버튼 클릭 여부
-        },
-        navigation: {   // 버튼
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-      });
-
-      const swiper = new Swiper('.swiper', {
-        // Optional parameters
-        direction: 'vertical',
-        loop: true,
+var mySwiper = new Swiper('.swiper-container', {
+  // 슬라이드를 버튼으로 움직일 수 있습니다.
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
       
-        // If we need pagination
-        pagination: {
-          el: '.swiper-pagination',
-        },
+  // 현재 페이지를 나타내는 점이 생깁니다. 클릭하면 이동합니다.
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+  
+  // 현재 페이지를 나타내는 스크롤이 생깁니다. 클릭하면 이동합니다.
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      draggable: true,
+    },
       
-        // Navigation arrows
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-      
-        // And if we need scrollbar
-        scrollbar: {
-          el: '.swiper-scrollbar',
-        },
-      });
+  // 3초마다 자동으로 슬라이드가 넘어갑니다. 1초 = 1000
+    autoplay: {
+      delay: 3000,
+    },
+  });
