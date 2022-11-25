3f9ad387943245b5ec09dc0a30449cafeda2da8d
SOYOUNGdev <soyoungim.sy@gmail.com>
2022-11-26 03:48:25 +0900

    파트너수정, -버튼활성화

--- dday/src/main/resources/static/product/js/category.js
+++ dday/src/main/resources/static/product/js/category.js
@@ -48,14 +48,31 @@ function countDown(){
     priceChange();
 }
 
+function countReset(){
+    var nowCount = $('div.cnt').text();
+    var minusReset = (((Number(nowCount))) -((Number(nowCount))- 1));
+
+    $('div.cnt').text(minusReset);
+
+    // 가격 변경 함수 호출
+    priceChange();
+}
+
+// +버튼 클릭시, 수량이 2이상이면 -버튼 활성화
 $('button.plus').on('click',function(){
+    $('button.minus').css({"background":"url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzAiIGhlaWdodD0iMzAiIHZpZXdCb3g9IjAgMCAzMCAzMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICAgIDxwYXRoIGQ9Ik0yMCAxNHYySDEwdi0yeiIgZmlsbD0iIzMzMyIgZmlsbC1ydWxlPSJub256ZXJvIi8+Cjwvc3ZnPgo=)"});
     countUp();
 });
 
+// -버튼 클릭시, 수량이 1이면 -버튼 다시 비활성화
 $('button.minus').on('click',function(){
     if(Number($('div.cnt').text()) > 1){
         countDown();
+        if(Number($('div.cnt').text()) == 1){
+            $('button.minus').css({"background":"url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzAiIGhlaWdodD0iMzAiIHZpZXdCb3g9IjAgMCAzMCAzMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICAgIDxwYXRoIGQ9Ik0yMCAxNHYySDEwdi0yeiIgZmlsbD0iI0RERCIgZmlsbC1ydWxlPSJub256ZXJvIi8+Cjwvc3ZnPgo=)"});
+        }
     }
+
 });
 
 
@@ -75,15 +92,18 @@ function priceChange(){
 
 // 장바구니 이미지 클릭시 모달창
 $('div.modalContainer').hide();
-    $('button.inputCartButton').on('click', function(){
+    $('button.inputCartButton').on('click', function(event){
+        event.stopPropagation();
         $("div.modalContainer").fadeIn();
     });
 
     $('button.modalOkButton').on('click', function(){
-        $('div.modalContainer').fadeOut();
-        // 장바구니쪽 작은 모달나오기
+        $('div.modalContainer').hide();
+        countReset();
+        alert('장바구니에 상품이 추가되었습니다.');
     });
 
     $('button.modalCancelButton').on('click',function(){
-        $('div.modalContainer').fadeOut();
+        $('div.modalContainer').hide();
+        countReset();
     });
