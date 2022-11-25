791f5b6ad6f7342b3dfa56671366950976aa0a24
devheesukang <heesukang2000@gmail.com>
2022-11-26 03:03:50 +0900

    퍼블리싱 끝냅시다...

--- Project/cart/js/cart.js
+++ Project/cart/js/cart.js
@@ -5,75 +5,156 @@
 const $checks = $(".check");
 const $checkAll = $("#checkAll");
 const $checkboxs = $(".check_content input[type=checkbox]");
+var itemcheck = 0;
 const $deletebutton = $(".choose_delete");
+var item_count = $(".productlist").length;
 
-$checkAll.on("click", function () {
-    console.log("checkAll");
-    console.log($checks.length);
-    if ($(this).is(":checked")) {
-        console.log("checked");
-        $checkboxs.next("span").addClass("fill");
-        $checkboxs.prop("checked", true);
-    } else {
-        $checkboxs.next("span").removeClass("fill");
-        $checkboxs.prop("checked", false);
-    }
-})
-/* $checkAll2.on("click", function () {
-    console.log("checkAll");
-    if ($(this).is(":checked")) {
-        $checkboxs.next("span").addClass("fill");
-        $checkboxs.prop("checked", true);
-    } else {
-        $checkboxs.next("span").removeClass("fill");
-        $checkboxs.prop("checked", false);
-    }
-}) */
+$(function () {
+    $(".item_count").text("(" + itemcheck + "/" + item_count + ")");
 
-
-$checks.on("click", function () {
-    let check = 0
-    if ($(this).is(":checked")) {
-        $(this).next("span").addClass("fill");
-
-    } else {
-        $(this).next("span").removeClass("fill")
-    }
-
-    $.each($checks, function () {
+    $checkAll.on("click", function () {
+        console.log("checkAll");
         if ($(this).is(":checked")) {
-            check += 1
+            console.log("checked");
+            $checkboxs.next("span").addClass("fill");
+            $checkboxs.prop("checked", true);
+            $deletebutton.prop('disabled', false);
+        } else {
+            $checkboxs.next("span").removeClass("fill");
+            $checkboxs.prop("checked", false);
+            $deletebutton.prop('disabled', true);
         }
+        itemcheck = ($('.check:checked').length);
+        $(".item_count").text("(" + itemcheck + "/" + item_count + ")");
     });
 
-    if (check == $checks.length) {
-        $checkAll.next("span").addClass("fill");
-        $checkAll.prop("checked", true);
-    } else {
-        $checkAll.next("span").removeClass("fill");
-        $checkAll.prop("checked", false);
-    }
 
-    if (check == 0) {
-        $deletebutton.prop('disabled', true);
-    } else {
-        $deletebutton.prop('disabled', false);
-    }
+    $checks.on("click", function () {
+        let check = 0;
+
+        if ($(this).is(":checked")) {
+            $(this).next("span").addClass("fill");
+        } else {
+            $(this).next("span").removeClass("fill")
+        }
+
+        $.each($checks, function () {
+            if ($(this).is(":checked")) {
+                check += 1
+            }
+        });
+
+
+        if (check == $checks.length) {
+            $checkAll.next("span").addClass("fill");
+            $checkAll.prop("checked", true);
+        } else {
+            $checkAll.next("span").removeClass("fill");
+            $checkAll.prop("checked", false);
+        }
+
+        if (check == 0) {
+            $deletebutton.prop('disabled', true);
+        } else {
+            $deletebutton.prop('disabled', false);
+        }
+
+        itemcheck = ($('.check:checked').length);
+        $(".item_count").text("(" + itemcheck + "/" + item_count + ")");
+    });
 });
 
+
+
+
 /* modal */
-$(function () {
 
 
-
-    $("button.choose_delete").click(function () {
-        $(".modal").fadeIn();
-        $('body').css("overflow", "hidden");
-    });
-
-    $(".modal_content").click(function () {
+$("button.delete_individual").click(function () {
+    let product = $(this).parent("li");
+    let temp = 0;
+    var delete_true = 0;
+    $(".modal").fadeIn();
+    $('body').css("overflow", "hidden");
+    temp = $(this).parent("li");
+    $(".confirm").click(function () {
+        product = temp;
+        product.remove();
         $(".modal").fadeOut();
         $('body').css("overflow", "visible");
+        delete_true = 1;
+        console.log(delete_true);
+        item_count = $(".productlist").length;
+        itemcheck = ($('.check:checked').length);
+        $(".item_count").text("(" + itemcheck + "/" + item_count + ")");
+    })
+    $(".cancel").click(function () {
+        $(".modal").fadeOut();
+        $('body').css("overflow", "visible");
+        delete_true = 0;
+        console.log(delete_true);
+        product = 0;
+    });
+    if (delete_true == 1) (function(){
+        item_count--;
+        console.log("item_count"+item_count);
+        $(".item_count").text("(" + itemcheck + "/" + item_count + ")");
+    });
+    $(".item_count").text("(" + itemcheck + "/" + item_count + ")");
+});
+
+
+$("button.choose_delete").click(function () {
+    let selected_product = $($(".check:checked").closest("li"));
+    $(".modal").fadeIn();
+    $('body').css("overflow", "hidden");
+    $(".confirm").click(function () {
+        selected_product = $($(".check:checked").closest("li"));
+        selected_product.remove();
+        $(".modal").fadeOut();
+        $('body').css("overflow", "visible");
+        item_count = $(".productlist").length;
+        itemcheck = ($('.check:checked').length);
+        $(".item_count").text("(" + itemcheck + "/" + item_count + ")");
+    })
+    $(".cancel").click(function () {
+        $(".modal").fadeOut();
+        $('body').css("overflow", "visible");
+        selected_product = 0;
     });
 
+    $(".item_count").text("(" + itemcheck + "/" + item_count + ")");
+});
+
+/* $(".cancel").click(function () {
+    $(".modal").fadeOut();
+    $('body').css("overflow", "visible");
+});
+$(".item_count").text("(" + itemcheck + "/" + item_count + ")"); */
+
+/* 주소 api */
+const $addressbutton = $(".addressbutton");
+const $addressdefault = $(".address_default");
+const $address_p = $(".address_p");
+const $address = $(".address_span");
+const $changeaddress = $(".changeaddress");
+const $searchaddress = $(".searchaddress");
+const $order_button = $(".order_button");
+const $address_false = $(".address_false");
+const $address_true = $(".address_true");
+
+$addressbutton.on("click", function () {
+    console.log("addressbutton");
+    new daum.Postcode({
+        oncomplete: function (data) { //선택시 입력값 세팅
+            $address.text(data.address); // 주소 넣기
+            $addressdefault.css('display', 'none');
+            $address_p.css('display', 'block');
+            $changeaddress.css('display', 'inline-block');
+            $searchaddress.css('display', 'none');
+            $order_button.prop('disabled', false);
+            $address_false.css('display', 'none');
+            $address_true.css('display', 'inline-block');
+        }
+    }).open();
 });
