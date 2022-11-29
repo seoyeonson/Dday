b4fe5e8082a6f164d7b397efee51408ce086fe53
JS8800gt <JS8800gt@gmail.com>
2022-11-29 17:53:43 +0900

    search 검

--- dday/src/main/java/com/example/dday/repository/SearchProductDAO.java
+++ dday/src/main/java/com/example/dday/repository/SearchProductDAO.java
@@ -1,9 +1,24 @@
 package com.example.dday.repository;
 
+import com.example.dday.domain.vo.ProductVO;
+import com.example.dday.mapper.SearchProductMapper;
 import lombok.RequiredArgsConstructor;
+import org.apache.ibatis.session.SqlSession;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class SearchProductDAO {
+    @Qualifier("sqlSessionTemplate")
+    @Autowired
+    private SqlSessionTemplate sqlSession;
+
+    public List<ProductVO> selectSearchList(ProductVO productVO) throws Exception{
+        return sqlSession.selectList("com.example.dday.mapper.SearchProductMapper.getProductList",productVO);
+    }
 }
